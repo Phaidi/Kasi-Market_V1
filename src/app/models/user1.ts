a78e1684cf32200fbd755a03ce3a620d3da11425
PHAIDI <phaidisamuel.ps@gmail.com>
2022-04-20 04:10:16 +0200

    init2

--- src/app/models/user1.ts
+++ src/app/models/user1.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 export class User1 {
     name: String | undefined;
+    surname: String | undefined;
     email: String | undefined;
     password: String | undefined;
     conPassword: String | undefined;
