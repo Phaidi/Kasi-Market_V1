9da52293cf4dbd5359803cc4be1c5a94bda3b014
PHAIDI <phaidisamuel.ps@gmail.com>
2022-06-17 00:42:31 +0200

    date

--- src/app/screens/customer/order/order.page.ts
+++ src/app/screens/customer/order/order.page.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/quotes */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,6 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class OrderPage implements OnInit {
 
   date = new Date();
+
+  countDownDate = new Date("June 23, 2022 15:37:25").getTime();
+  demo: any;
+
+  x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = this.countDownDate - now;
+    var days = Math.floor(distance /(1000*60*60*24));
+    var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((distance % (1000*60)) / 1000);
+    this.demo = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  });
 
   constructor() { }
 
